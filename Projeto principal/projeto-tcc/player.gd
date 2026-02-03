dfeca41c641d29402742a08fa3b8834c2ac46ff1
Marcio Salmazo <marcio.salmazo19@gmail.com>
2026-02-03 17:51:55 -0300

    Inicio do desenvolvimento da Godot

---
+++ Projeto principal/projeto-tcc/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+@onready var anxiety_controller = get_parent().get_node("AnxietyController")
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var ansiedade = anxiety_controller.ansiedade
+	var instabilidade = randf_range(-ansiedade, ansiedade)
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = (direction * SPEED) + instabilidade
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
