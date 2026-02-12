77085c509306edda8ba4bdc5a165de436b3f0e2f
Marcio Salmazo <marcio.salmazo19@gmail.com>
2026-02-12 10:00:49 -0300

    Refinamento Físico da Fase inicial

--- Projeto principal/projeto-tcc/anxiety_controller.gd
+++ Projeto principal/projeto-tcc/anxiety_controller.gd
@@ -1,13 +1,25 @@
+# Define que o Script opere com o objeto do tipo Node (Genérico)
 extends Node
+# @onready significa que essa variável só será atribuída quando o nó já estiver pronto na cena
 @onready var heartbeat = get_parent().get_node("Heartbeat")
 
+# @export faz a variável aparecer no Inspector da Godot.
+# Em resumo, podemos mudar o valor direto na interface do editor, sem mexer no código.
+# OBS: o operador := significa declaração com tipagem automática (type inference)
 @export var ansiedade := 0.0
 @export var taxa_crescimento := 0.5
 @export var ansiedade_max := 20.0
 
+# Função chamada a cada frame, de acordo com a taxa de quadros do jogo. 
+# É usada para tudo que depende de tempo contínuo, como animações, efeitos visuais, ou contadores.
+# O parâmetro delta representa o tempo decorrido desde o último frame.
 func _process(delta):
+	
+	# Define um acrescimo ao parâmetro de ansiedade de acordo com o tempo decorrido
 	ansiedade += taxa_crescimento * delta
+	# Define os limites minimos e máximos da ansiedade
 	ansiedade = min(ansiedade, ansiedade_max)
+	# log
 	print(ansiedade)
 	# O som acelera progressivamente de acordo com o grau de ansiedade
 	heartbeat.pitch_scale = 1.0 + (ansiedade * 0.1)
--- Projeto principal/projeto-tcc/player.gd
+++ Projeto principal/projeto-tcc/player.gd
@@ -1,28 +1,91 @@
+# Define que o Script opere com o objeto do tipo CharacterBody2D
 extends CharacterBody2D
+
+# @onready significa que essa variável só será atribuída quando o nó já estiver pronto na cena
+# var anxiety_controller recebe os dados do nó (genérico) AnxietyController
 @onready var anxiety_controller = get_parent().get_node("AnxietyController")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Atribuição de valores constantes e estáticos para velocidade, gravidade e pulo
+const SPEED := 300.0
+const JUMP_VELOCITY := -400.0
 
+# Função responsável por processar a física do player
+# A função é chamada em um intervalo fixo, sincronizado com o sistema de física da engine
+# O parâmetro delta representa o tempo decorrido desde o último frame.
+# Essa função é responsável por tudo que envolve movimento físico, colisão ou forças.
 func _physics_process(delta: float) -> void:
 	
-	# Add the gravity.
-	if not is_on_floor():
+	# ============================================================================================ #
+	# 								CONTROLE DA GRAVIDADE
+	# ============================================================================================ #
+
+	# Evita perda de contato com o chão, impedindo pequenos saltos ao mudar de inclinação.
+	# Sempre que o player estuver em contado com o chão, a velocidade vertical não muda
+	if is_on_floor():
+		velocity.y = 0
+	# Evita que o personagem acumule velocidade vertical indevida.
+	else:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# ============================================================================================ #
+	# 								CONTROLE DE PULO
+	# ============================================================================================ #
+	# Só funciona se o botão de ação for pressionado e se o jogador estiver em contato com o chão
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		# Define a velocidade no eixo Y para efetuar a propulsão do pulo
 		velocity.y = JUMP_VELOCITY
 	
+	# ============================================================================================ #
+	# 							CONTROLE DA TAXA DE INSTABILIDADE
+	# ============================================================================================ #
+	# Recebe o valor de ansiedade
 	var ansiedade = anxiety_controller.ansiedade
+	# Define a distorção do controle (Representado pela Ansiedade)
 	var instabilidade = randf_range(-ansiedade * 10, ansiedade * 10)
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# ============================================================================================ #
+	# 									MOVIMENTAÇÃO EFETIVA
+	# ============================================================================================ #
+	# A variável recebe o valor de entrada dos botões de comando (Definidos por padrão)
+	# OBS: o operador := significa declaração com tipagem automática (type inference)
+	#      Ele cria a variável e já define o tipo dela automaticamente com base no valor atribuído.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Caso seja definido uma valor para direction:
 	if direction:
+		# A velocidade horizontal é aplicada considerando a velocidade definida e a instabilidade
 		velocity.x = (direction * SPEED) + instabilidade
+	# Caso não seja definido uma valor para direction:
 	else:
+		# A velocidade horizontal decrescida ao passo da velocidade
+		# "Leva o valor atual (velocity.x) até o valor alvo (0) a um passo definido por SPEED"
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Redundância para ativar 'Stop on Slope' e 'Constant Speed' no inspetor.
+	# Definem como o personagem se comporta no chão, considerando a inclinação
+	# Stop on Slope -> Impede que o personagem escorregue sozinho em rampas quando está parado.
+	# Constant Speed -> Mantém a velocidade constante ao andar em rampas.
+	set_floor_stop_on_slope_enabled(true)
+	set_floor_constant_speed_enabled(true)
+	# OBSERVAÇÃO (Inspetor do CharacterBody2D):
+	# Max Angle -> Angulo máximo que a engine considera a inclinação como 'chão'
+	# Snap Length -> Faz o personagem “grudar” levemente no chão, evitando micro saltos.
+	
+	# ============================================================================================ #
+	# 						CONTROLE DE ROTAÇÃO DO PERSONAGEM COM O CHÃO
+	# ============================================================================================ #
+	
+	if is_on_floor():
+		# Pega o valor normal da superfície
+		var normal = get_floor_normal()
+		# Define a rotação-alvo para o personagem de acordo com a normal da superfície
+		var target_rotation = normal.angle() + PI/2
+		# Aplica a rotação, de modo interpolada (garante suavidade).
+		rotation = lerp_angle(rotation, target_rotation, 10 * delta)
+	else:
+		# Se o personagem não estiver em contato com o chão, a rotão retorna a 0
+		rotation = 0
+	
+	# Função usada principalmente em CharacterBody2D (ou CharacterBody3D) para
+	# mover o personagem aplicando colisão e fazendo ele deslizar nas superfícies automaticamente.
 	move_and_slide()
